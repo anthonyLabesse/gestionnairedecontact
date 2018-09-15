555207189fe07dccd82ecfd70e66ffbc69b94ee5
Anthony Labesse <labesseanthony63@gmail.com>
2018-09-15 14:30:05 +0200

    Correction du bug du deuxieme affichage

--- js/gestion.js
+++ js/gestion.js
@@ -12,16 +12,27 @@ var Contact = {
 // Méthode de l' objet contact qui permet d'afficher les contacts de la liste contact
     
     afficherContact: function(){
-        var affichage = "Nom: " + this.nom.toUpperCase() + "\t" + " Prénom: " + this.prenom;
+        contact.forEach(function(Contact){
+            console.log(Contact.afficher())
+        });
+        
+    },
+    
+    afficher: function(){
+        var affichage = "Prénom: " + this.nom.toUpperCase() + " " + "Nom: " + this.prenom;
         return affichage;
     },
     
 // Méthode qui permet d'ajouter un contact à la liste
     
     ajouterContact: function(nom, prenom){
-        newcontact.init(nom, prenom);
+        var newcontact = Object.create(Contact);
+        var newnom = prompt("Veuillez saisir le nom de votre contact");
+        var newprenom = prompt("Veuillez saisir le prénom de votre contact");
+        newcontact.init(newnom, newprenom);
         contact.push(newcontact);
-    }
+        console.log("Votre contact " + newnom.toUpperCase() + " " + newprenom + " a bien été ajouté." );
+    },
 };
 
 
@@ -33,7 +44,7 @@ contact1.init("Lewis", "Caroll");
 var contact2 = Object.create(Contact);
 contact2.init("Manson", "Marilyn");
 
-var newcontact = Object.create(Contact);
+
 
 // Affichage de la console 
 
@@ -58,18 +69,14 @@ do{
         break;
         
     case "1":
-        contact.forEach(function(Contact){
-            console.log(Contact.afficherContact());
-        })
+        Contact.afficherContact();
         break;
     
     case "2":
-        var newnom = prompt("Veuillez saisir le nom de votre contact");
-        var newprenom = prompt("Veuillez saisir le prénom de votre contact");
-        newcontact.ajouterContact();
-        console.log("Votre contact " + newnom.toUpperCase() + " " + newprenom + " a bien été ajouté." );
+        Contact.ajouterContact();
+        break;
     }
-}while(choix !== 0);
+}while(choix != 0);
 
 
 
