57ed219342de6984d86468c3e73db84304375aae
Anthony Labesse <labesseanthony63@gmail.com>
2018-09-15 02:15:15 +0200

    Ajout de la boite d'option

--- js/gestion.js
+++ js/gestion.js
@@ -3,9 +3,12 @@ Activité : gestion des contacts
 */
 
 // TODO : complétez le programme
+/*
 console.log("Bienvenue dans le gestionnaire de contact ");
 console.log(" 1 : Lister les contacts ");
 console.log(" 2 : Ajouter un contact ");
 console.log(" 0 : Quitter ");
+*/
 
+var choix = prompt(Quel est votre choix ?);
 
