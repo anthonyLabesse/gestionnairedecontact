1ef2d025c8b1d8bd5ffcf22aade745ca8ce896b7
Anthony Labesse <labesseanthony63@gmail.com>
2018-09-15 11:11:53 +0200

    Ajout de la boucle do while

--- js/gestion.js
+++ js/gestion.js
@@ -2,60 +2,73 @@
 Activité : gestion des contacts
 */
 
-// TODO : complétez le programme
-/*
-
-*/
-
-// Initialise le contact
+// Initialisation du contact
 var Contact = {
     init: function(nom, prenom){
         this.nom = nom;
         this.prenom = prenom;
     },
     
+// Méthode de l' objet contact qui permet d'afficher les contacts de la liste contact
+    
     afficherContact: function(){
         var affichage = "Nom: " + this.nom.toUpperCase() + "\t"+ " Prénom: " + this.prenom;
         return affichage;
     },
     
-// Permet de fermer la fenêtre en cours
-    fermerFenetre: function(){
-        window.close();
+// Méthode qui permet d'ajouter un contact à la liste
+    
+    ajouterContact: function(nom, prenom){
+        newcontact.init(nom, prenom);
+        contact.push(newcontact);
     }
 };
 
 
+// Instanciation des objets
 
 var contact1 = Object.create(Contact);
-contact1.init("Lévisse", "Carole");
+contact1.init("Lewis", "Caroll");
 
 var contact2 = Object.create(Contact);
-contact2.init("Nelson", "Mélodie");
+contact2.init("Manson", "Marilyn");
+
+var newcontact = Object.create(Contact);
 
 console.log("Bienvenue dans le gestionnaire de contact ");
 console.log(" 1 : Lister les contacts ");
 console.log(" 2 : Ajouter un contact ");
 console.log(" 0 : Quitter ");
 
-var choix = prompt("Quel est votre choix ?");
+
 var contact = [];
 contact.push(contact1);
 contact.push(contact2);
 
 
-switch(choix){
-    case "1":
-    contact.forEach(function(Contact){
-        console.log(Contact.afficherContact());
-    });
-    break;
-    
+// Switch qui permet de faire les choix d'option avec une boucle do while 
+
+do{
+    var choix = prompt("Quel est votre choix ?");
+    switch(choix){
     case "0":
-    console.log("Au revoir");
-    Contact.fermerFenetre();
-    break;
-}
+        console.log("Au revoir");
+        break;
+        
+    case "1":
+        contact.forEach(function(Contact){
+            console.log(Contact.afficherContact());
+        });
+        break;
+    
+    case "2":
+        var newnom = prompt("Veuillez saisir le nom de votre contact");
+        var newprenom = prompt("Veuillez saisir le prénom de votre contact");
+        newcontact.ajouterContact();
+        console.log("Votre contact " + newnom.toUpperCase() + " " + newprenom + " a bien été ajouté." );
+    }
+}while:(choix !== 0);
+
 
 
 
